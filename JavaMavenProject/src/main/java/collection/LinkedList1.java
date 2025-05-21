6416852aa048b0253435a07226fd8fbb36522376
dyuth <dyuth@192.168.1.40>
2025-05-21 23:14:27 +0530

    new commit

---
+++ JavaMavenProject/src/main/java/collection/LinkedList1.java
@@ -0,0 +1,22 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LinkedList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String> obj =new LinkedList<String>();
+		obj.add("India");
+		obj.add("France");
+		obj.add("China");
+		obj.add("London");
+		obj.add("Dubai");
+		
+		System.out.println(obj);
+		
+
+	}
+
+}
--- JavaMavenProject/src/main/java/excelread/ExcelRead.java
+++ JavaMavenProject/src/main/java/excelread/ExcelRead.java
@@ -1,7 +1,10 @@
 package excelread;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -10,5 +13,27 @@ public class ExcelRead {
 	public static FileInputStream fs;
 	public static XSSFWorkbook w;
 	public static XSSFSheet s;
+	public static String readstringdata(int a,int b) throws IOException {
+		
+		fs =new FileInputStream("C:\\Users\\dyuth\\Documents\\Java Programs\\SalarySample.xlsx");
+		w = new XSSFWorkbook(fs);
+		s=w.getSheet("Sheet1");
+		XSSFRow r =s.getRow(a);
+		XSSFCell c = r.getCell(b);
+		return c.getStringCellValue();
+		
+	}
+	
+public static String readintegerdata(int a,int b) throws IOException {
+		
+		fs =new FileInputStream("C:\\Users\\dyuth\\Documents\\Java Programs\\SalarySample.xlsx");
+		w = new XSSFWorkbook(fs);
+		s=w.getSheet("Sheet1");
+		XSSFRow r =s.getRow(a);
+		XSSFCell c = r.getCell(b);
+	   int x = (int) c.getNumericCellValue();
+	   return String.valueOf(x);
+		
+	}
 
 }
